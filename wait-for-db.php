dffcc8ab5f19062f1f835ce5ff34680d7eeff9d1
Nabil Kadimi <nabil@kadimi.com>
2022-11-14 04:34:30 +0100

    Fix wait-for-db.php

--- wait-for-db.php
+++ wait-for-db.php
@@ -4,19 +4,21 @@ shell_exec('php setup.php db-port');
 $db_port = trim(file_get_contents('db-port'));
 
 $max_tries = 60;
-echo 'Waiting for database';
+echo 'Waiting for database' . "\n";
 do {
-    set_error_handler(function() {});
-    $mysql = new mysqli("0.0.0.0", 'wordpress', 'wordpress', 'wordpress', $db_port);
-    restore_error_handler();
-    if ($mysql->connect_error) {
+    try {
+        set_error_handler(function() {});
+        $mysqli = new mysqli("0.0.0.0", 'wordpress', 'wordpress', 'wordpress', $db_port);
+        restore_error_handler();
+        break;
+    } catch (\Throwable $th) {
         --$max_tries;
-        if ($max_tries <= 0) {
-            echo ' - Database not responding' . "\n";
+        if ($max_tries < 0) {
+            echo "\n" . 'Database not responding';
             exit(1);
         }
-        echo '.';
+        echo '-';
         sleep(1);
     }
-} while ($mysql->connect_error);
-echo ' - Database ready' . "\n";
+} while (true);
+echo "\n" . 'Database ready';
